e540261004a8492d9f763f4f04e671c6090216a8
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-05-17 17:03:48 +0300

    write base data in file

--- __main__.py
+++ __main__.py
@@ -1,6 +1,11 @@
-from codage.file import read_data
+from codage.file import read_data, write_huffman_data
+from codage import  huffman_base
 
 print("Ceci est un projet d'evaluation des connaissances en codage")
 
 ## lecture du fichier text generer
 text_data = read_data(filepath="2434.txt")
+## Codage de huffman
+M,S,P = huffman_base(text_data)
+write_huffman_data('_huff_data.txt',M,S,P)
+
