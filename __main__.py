3ec6239e1225461e6f0f03dae94c3b7c5a98f91a
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-05-17 16:48:58 +0300

    move data to 2434 file

--- __main__.py
+++ __main__.py
@@ -3,5 +3,4 @@ from codage.file import read_data
 print("Ceci est un projet d'evaluation des connaissances en codage")
 
 ## lecture du fichier text generer
-text_data = read_data(filepath="text.txt")
-
+text_data = read_data(filepath="2434.txt")
